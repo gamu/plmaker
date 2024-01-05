759ebbd98c2033df82aa9e3f8d3419e0c8090ffe
Ivan Grishin <igrishin@iegd.ru>
2024-01-05 23:07:50 +0400

    Homework with intents

--- app/src/main/java/ru/gamu/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/ru/gamu/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,8 @@
 package ru.gamu.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,6 +15,36 @@ class SettingsActivity: AppCompatActivity() {
         btnBack.setOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
+        }
+
+        val btnShare = findViewById<ImageView>(R.id.btnShare)
+        btnShare.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.setType("text/plain")
+            val message = getString(R.string.praktikumUrl)
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(Intent.createChooser(intent, getString(R.string.optionsMessage)))
+        }
+
+        val btnSupport = findViewById<ImageView>(R.id.btnSupport)
+        btnSupport.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            val mailTo = getString(R.string.mailTo)
+            intent.data = Uri.parse("mailto:${mailTo}")
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mailTheme))
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.mailBody))
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }
+        }
+
+        val btnBrowser = findViewById<ImageView>(R.id.btnBrowser)
+        btnBrowser.setOnClickListener {
+            val uri = Uri.parse(getString(R.string.agreementUrl))
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }
         }
     }
 }
